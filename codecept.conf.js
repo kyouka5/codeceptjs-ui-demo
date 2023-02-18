69f169493cd4a64b6de23ace8bc3758232b3a8d2
Márta Nagy <kyoukaa98@gmail.com>
2023-02-18 01:38:09 +0100

    add config for allure report

--- codecept.conf.js
+++ codecept.conf.js
@@ -1,53 +1,52 @@
 exports.config = {
-  output: './output',
-  helpers: {
-    AssertWrapper: {
-      require: "codeceptjs-assert"
+    output: './allure-results',
+    helpers: {
+        AssertWrapper: {
+            require: "codeceptjs-assert"
+        },
+        Playwright: {
+            url: 'https://github.com',
+            show: true,
+            browser: 'chromium',
+            bypassCSP: true
+        }
     },
-    Playwright: {
-      url: 'https://github.com',
-      show: true,
-      browser: 'chromium',
-      bypassCSP: true
-    }
-  },
-  include: {
-    I: './steps_file.js',
-    pages: './pages/pages.js',
-    homePage: './pages/home.js',
-    constants: './constants.js'
-  },
-  mocha: {
-    reporterOptions: {
-      mochaFile: 'output/test-report.xml'
-    }
-  },
-  bootstrap: null,
-  timeout: null,
-  teardown: null,
-  hooks: [],
-  gherkin: {
-    features: './features/*.feature',
-    steps: ['./step_definitions/steps.js']
-  },
-  plugins: {
-    screenshotOnFail: {
-      enabled: true
+    include: {
+        I: './steps_file.js',
+        pages: './pages/pages.js',
+        homePage: './pages/home.js',
+        constants: './constants.js'
     },
-    retryFailedStep: {
-      enabled: true
-    }
-  },
-  stepTimeout: 0,
-  stepTimeoutOverride: [{
-      pattern: 'wait.*',
-      timeout: 0
+    mocha: {
+        reporterOptions: {
+            mochaFile: 'output/test-report.xml'
+        }
     },
-    {
-      pattern: 'amOnPage',
-      timeout: 0
-    }
-  ],
-  tests: './test/*_test.js',
-  name: 'github-ui-demo'
+    bootstrap: null,
+    timeout: null,
+    teardown: null,
+    hooks: [],
+    gherkin: {
+        features: './features/*.feature',
+        steps: ['./step_definitions/steps.js']
+    },
+    plugins: {
+        allure: {},
+        retryFailedStep: {
+            enabled: true,
+            defaultIgnoredSteps: [],
+        },
+    },
+    stepTimeout: 0,
+    stepTimeoutOverride: [{
+        pattern: 'wait.*',
+        timeout: 0
+    },
+        {
+            pattern: 'amOnPage',
+            timeout: 0
+        }
+    ],
+    tests: './test/*_test.js',
+    name: 'github-ui-demo'
 }
